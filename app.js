be8835b44d5b2d91a53087e6ad06de015ae59141
GwangseongCho <jgs9904@gmail.com>
2022-06-02 00:46:27 +0900

    깃 저장소 초기화

---
+++ app.js
@@ -0,0 +1,19 @@
+"use strict";
+
+//modules 
+const express = require("express");
+const app = express();
+
+//router
+const home = require("./routes/home");
+
+//port
+const port = 3000;
+
+//app setting
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
+app.use("/", home); //use -> 미들 웨어를 등록해주는 메소드.
+
+module.exports = app;
